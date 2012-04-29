a8b69e2592dfd297e1d5b54224d8e3979130db25
MichaelDavid Pascual <mdnpascual@yahoo.com.ph>
2012-04-28 22:01:40 -0600

    First Commit

---
+++ Dota2CLS/Dota2CLS.Designer.vb
@@ -0,0 +1,144 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Dota2CLS
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(13, 98)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.TextBox1.Size = New System.Drawing.Size(1007, 451)
+        Me.TextBox1.TabIndex = 0
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Enabled = False
+        Me.TextBox2.Location = New System.Drawing.Point(132, 17)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(525, 20)
+        Me.TextBox2.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(12, 16)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(114, 18)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Demo File Path:"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(663, 15)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(69, 23)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "Open"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Button2)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Location = New System.Drawing.Point(814, 13)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(206, 79)
+        Me.GroupBox1.TabIndex = 4
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Details"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(31, 24)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(39, 13)
+        Me.Label2.TabIndex = 0
+        Me.Label2.Text = "Label2"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(125, 19)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Show Me"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(31, 53)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(39, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Label3"
+        '
+        'Dota2CLS
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1032, 561)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Name = "Dota2CLS"
+        Me.Text = "Dota 2 Combat Log Simulator"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents OpenFileDialog1 As System.Windows.Forms.OpenFileDialog
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+
+End Class
---
+++ Dota2CLS/Dota2CLS.vb
@@ -0,0 +1,95 @@
+﻿
+Public Class Dota2CLS
+
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        OpenFileDialog1.Title = "Please Select a File"
+        OpenFileDialog1.InitialDirectory = "C:\Program Files (x86)\Steam\steamapps\common\dota 2 beta\dota\replays" 'Default Location
+
+        OpenFileDialog1.ShowDialog()
+
+    End Sub
+
+    Private Sub OpenFileDialog1_FileOk(ByVal sender As System.Object, ByVal e As System.ComponentModel.CancelEventArgs) Handles OpenFileDialog1.FileOk
+        Dim sr As New IO.StreamReader(OpenFileDialog1.OpenFile())                                               'Stream Reader to Read the large .txt
+        TextBox2.Text = OpenFileDialog1.FileName.ToString()                                                     'Puts the Directory of the .txt in the disabled textbox
+
+
+        ''''''''''''''''''''''''''Fetch CombatLogNames''''''''''''''''''''''''''
+        Do Until sr.EndOfStream = True                                                                          'Read whole File
+            Dim streambuff As String = sr.ReadLine                                                              'Array to Store CombatLogNames
+            Dim CombatLogNames() As String
+
+            If streambuff.Contains("CombatLogNames flags:0x1") Then                                             'Keyowrod to Filter CombatLogNames Packets in the .txt
+
+                Dim check As String = streambuff                                                                'Duplicate of the Line being read
+                Dim index1 As Char = check.Substring(check.IndexOf("(") + 1)                                    'v
+                Dim index2 As Char = check.Substring(check.IndexOf("(") + 2)                                    'Used to bypass the first CombatLogNames packet that contain only 1 entry
+
+                If (check.IndexOf("(") <> -1 And index1 <> "" And index2 <> " ") Then                           'Stricter Filters for CombatLogNames
+
+                    Dim endCLN As Integer = 0                                                                   'Signifies the end of CombatLogNames Packet
+                    Dim x As Integer = 0                                                                        'Counter for array
+
+                    While (endCLN = 0 And streambuff <> "---- CNETMsg_Tick")                                    'Loops until the end keyword for CombatLogNames is seen
+
+                        streambuff = sr.ReadLine                                                                'Reads a new line to flush out "CombatLogNames flags:0x1" which is unneeded
+                        If (streambuff.Contains("---- CNETMsg_Tick") = True) Then
+
+                            endCLN = 1                                                                          'Value change to determine end of CombatLogName packet
+
+                        Else
+
+                            ReDim Preserve CombatLogNames(x)                                                    'Resizes the array while preserving the values
+                            CombatLogNames(x) = streambuff.Trim.Remove(streambuff.IndexOf("(") - 5).Remove(0, streambuff.Trim.Remove(streambuff.IndexOf("(")).IndexOf("'")) 'Additional filtering to get only valuable data
+                            'String is Removed of Spaces (trim) then removed "0 bytes" (first remove) and removal of #xx value (2nd remove)
+                            x += 1                                                                              '+1 to Array counter
+
+                        End If
+                    End While
+                Else
+                    'MsgBox("Something went wrong, Flame the coder of this program!!")                          'Bug Testing code that is disabled
+                End If
+
+            End If
+
+            If (sr.EndOfStream = True) Then                                                                     'Used to flush CombatLogNames list in GUI
+
+                Dim x = 0                                                                                       'Counter
+
+                While (x <> CombatLogNames.Length)                                                              'Loops the whole array
+
+                    'TextBox1.AppendText("#" & x & ": " & CombatLogNames(x) & vbNewLine)                         'Displays the string in textbox 
+                    x += 1
+
+                End While
+            End If
+        Loop
+        ''''''''''''''''''''''''''Fetch CombatLogNames''''''''''''''''''''''''''
+        sr.Close()
+        ''''''''''''''''''''''''''Fetch CombatLogEntries''''''''''''''''''''''''''
+        '["timestamp"] "attackername" hits "targetname" with "inflictorname" for "value" damage (???->"health")
+
+        Dim sr2 As New IO.StreamReader(OpenFileDialog1.OpenFile())
+
+        Dim streambuff As String = ""
+        Dim CombatLogData() As String
+        Dim Before1 As String = ""
+        Dim Keyword As String = ""
+
+        Do Until sr.EndOfStream = True
+
+            Before1 = streambuff
+            streambuff = sr.ReadLine()
+
+            If streambuff.Contains("name: ""dota_combatlog""") Then
+
+
+            End If
+
+
+        Loop
+
+
+    End Sub
+End Class
